433b2303e6ed3d0da01760c9cfb3fbe01c5a41dd
Renee Gillilan <reneegillilan@gmail.com>
2025-04-06 11:07:22 -0400

    Auth design done

--- loot-seton/src/Components/Auth/AuthForm.jsx
+++ loot-seton/src/Components/Auth/AuthForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Parse from "parse";
 import { Navigate, useNavigate } from "react-router-dom";
+import "./Auth.css";
 
 const AuthForm = ({ user, onChange, isLogin, onSubmit }) => {
   const navigate = useNavigate();
@@ -13,68 +14,72 @@ const AuthForm = ({ user, onChange, isLogin, onSubmit }) => {
 
   //display to login or register for the service
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        {!isLogin ? 
-        <div>
-        <div>
-          <label>First Name</label>
-          <br />
-          <input
-            type="text"
-            value={user.firstName}
-            onChange={onChange}
-            name="firstName"
-            placeholder="first name"
-            required
-          />
+    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh', width: '100vw'}}>
+      <div className="border">
+        <div className="innerBox">
+          <form className="form-design" onSubmit={onSubmit}>
+            {!isLogin ? 
+            <div>
+            <div>
+              <label>First Name</label>
+              <br />
+              <input
+                type="text"
+                value={user.firstName}
+                onChange={onChange}
+                name="firstName"
+                placeholder="first name"
+                required
+              />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <br />
+              <input
+                type="text"
+                value={user.lastName}
+                onChange={onChange}
+                name="lastName"
+                placeholder="last name"
+                required
+              />
+            </div>{" "}
+            </div> : <></>}
+            <div>
+              <label>Email</label>
+              <br />
+              <input
+                type="email"
+                value={user.email}
+                onChange={onChange}
+                name="email"
+                placeholder="email"
+                required
+              />
+            </div>{" "}
+            <div>
+              <label>Password</label>
+              <br />
+              <input
+                type="password"
+                value={user.password}
+                onChange={onChange}
+                name="password"
+                placeholder="password"
+                min="0"
+                required
+              />
+            </div>
+            <div className="button2">
+              <button class="btn btn-outline-dark custom-btn2" type="submit" onSubmit={onSubmit}>
+                Submit
+              </button>
+            </div>
+          </form>
+          <div className="button2">
+            <button class="btn btn-outline-dark custom-btn2" onClick={goBackHandler}>Back</button>
+          </div>
         </div>
-        <div>
-          <label>Last Name</label>
-          <br />
-          <input
-            type="text"
-            value={user.lastName}
-            onChange={onChange}
-            name="lastName"
-            placeholder="last name"
-            required
-          />
-        </div>{" "}
-        </div> : <></>}
-        <div>
-          <label>Email</label>
-          <br />
-          <input
-            type="email"
-            value={user.email}
-            onChange={onChange}
-            name="email"
-            placeholder="email"
-            required
-          />
-        </div>{" "}
-        <div>
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            value={user.password}
-            onChange={onChange}
-            name="password"
-            placeholder="password"
-            min="0"
-            required
-          />
-        </div>
-        <div>
-          <button type="submit" onSubmit={onSubmit}>
-            Submit
-          </button>
-        </div>
-      </form>
-      <div>
-        <button onClick={goBackHandler}>Back</button>
       </div>
     </div>
   );

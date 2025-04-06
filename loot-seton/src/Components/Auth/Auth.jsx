1638d7b809af19c6315aeb2e17a08464b07b9172
Renee Gillilan <reneegillilan@gmail.com>
2025-04-06 08:39:48 -0400

    added bootstrap, started designing auth

--- loot-seton/src/Components/Auth/Auth.jsx
+++ loot-seton/src/Components/Auth/Auth.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Parse from "parse";
 import { Link } from "react-router-dom";
+import './Auth.css'
 
 const AuthModule = () => {
   //display to select register/login
   return (
-    <div>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
-      <br />
-      <br />
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh', width: '100vw'}}>
+      <div className="outerBox">
+        <div>
+          <h1>Loot Seton</h1>
+        </div>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+        <br />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+      </div>
     </div>
   );
 };

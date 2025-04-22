26e14a63dd70c6ce42fa9c166bd1493d8e534d24
Renee Gillilan <reneegillilan@gmail.com>
2025-04-22 16:46:08 -0400

    home design still WIP

--- loot-seton/src/Components/Home/HomeEditForm.jsx
+++ loot-seton/src/Components/Home/HomeEditForm.jsx
@@ -1,21 +1,26 @@
 const HomeEditForm = ({onChangeF, onChangeN, onClick, onBack, onSelectF, onSelectN, folderTitle, noteStuff}) => {
     return (
         <div className="editPage">
-            <div className="editTitle">
-                <h2>Edit</h2>
+            <div className="editTop">
+                <div className="editTitle">
+                    <h2>Edit</h2>
+                </div>
+                <div className="closeEdit">
+                    <button onClick={onBack}>Close</button>
+                </div>
             </div>
             <div>
                 <form>
                     {onSelectF ? 
-                    <div>
-                        <div>
+                    <div className="editFolder">
+                        <div className="prevFolder">
                             Folder Selected: 
                             <br/>
                             {folderTitle}
                         </div>
                         <label htmlFor="folderName">Edit Folder Name Here:</label>
                         <br/>
-                        <input type="text"  id="folderName" maxLength={20} onChange={onChangeF}></input>
+                        <input type="text"  id="folderName" maxLength={15} onChange={onChangeF}></input>
                     </div>: <></>}
                     {onSelectN ? 
                     <div>
@@ -36,13 +41,10 @@ const HomeEditForm = ({onChangeF, onChangeN, onClick, onBack, onSelectF, onSelec
                           className="note-textbox"
                         ></textarea>
                     </div> : <></>}
-                    <div>
+                    <div className="editSubmit">
                         <button type="submit" onClick={onClick}>Submit</button>
                     </div>
                 </form>
-            </div>
-            <div>
-                <button onClick={onBack}>Close</button>
             </div>
         </div>
     )

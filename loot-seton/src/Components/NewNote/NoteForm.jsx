9917887035140435d469ac0868f225c3cbd88c56
Rose34663789 <megkatfin@gmail.com>
2025-04-24 16:15:23 -0400

    made class based instead of html element

--- loot-seton/src/Components/NewNote/NoteForm.jsx
+++ loot-seton/src/Components/NewNote/NoteForm.jsx
@@ -3,19 +3,19 @@ import "./Note.css"
 const NoteForm = ({onChangeF, onChangeN, onChangeU, onClick}) => {
     return (
         <div>
-            <form className="form-switch">
-                <label >Folder Name</label>
+            <form className="form-switch newNoteForm">
+                <label className="newNoteLabel">Folder Name</label>
                 <br />
-                <input type="text" id="folderName" onChange={onChangeF} maxLength={15}></input>
+                <input type="text" className="newNoteInput" id="folderName" onChange={onChangeF} maxLength={15}></input>
                 <br />
-                <label >Note</label>
+                <label className="newNoteLabel">Note</label>
                 <br />
-                <textarea id="noteContent" className="newNoteNote" onChange={onChangeN} maxLength={100}></textarea>
+                <textarea id="noteContent" className="newNoteInput newNoteNote" onChange={onChangeN} maxLength={100}></textarea>
                 <br />
                 <br />
                 <div className="checkboxStyle">
-                <label>Make Private?</label>
-                <input type="checkbox" className="checkbox form-check-input" id="user" onChange={onChangeU}></input>
+                <label className="newNoteLabel">Make Private?</label>
+                <input type="checkbox" className="newNoteInput checkbox form-check-input" id="user" onChange={onChangeU}></input>
                 </div>
                 <br />
                 <button class="submitButton" type="submit" onClick={onClick}>Create</button>
